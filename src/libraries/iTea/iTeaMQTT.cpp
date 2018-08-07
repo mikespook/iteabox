bf23b2db630ac99b244c3518dddd36da88428618
Xing Xing <mikespook@gmail.com>
2018-08-07 15:25:04 +1200

    added MQTT class

---
+++ src/libraries/iTea/iTeaMQTT.cpp
@@ -0,0 +1,75 @@
+#include "iTeaMQTT.h"
+
+// Public
+iTeaMQTTClass::iTeaMQTTClass() {
+}
+
+void iTeaMQTTClass::init(iTeaConfigClass *config, CallbackHandler callback) {
+	_config = config;
+	_callback = callback;
+}
+
+uint8_t iTeaMQTTClass::setup() {
+	char addr[ITEA_MQTT_ADDR_SIZE + 1] = {0};
+	_config->getMQTTAddr(addr);
+	char port[ITEA_MQTT_PORT_SIZE + 1] = {0};
+	_config->getMQTTPort(port);	
+	_mqtt.setServer(addr, atoi(port));
+	// heartbeat
+	_config->getMQTTTopicHeartbeat(_pubTopicHeartbeat);
+	char tick[ITEA_MQTT_HBTICK_SIZE + 1] = {0};
+	_config->getMQTTHeartbeatTick(tick);
+	_heartbeatTick = atoi(tick);
+
+	// topics
+	_config->getMQTTTopic(_subTopic);
+
+	_config->getMQTTClientId(_clientId);
+	_config->getMQTTUser(_user);
+	_config->getMQTTPass(_pass);
+
+	_mqtt.setCallback([this](char* topic, uint8_t* payload, unsigned int length){
+		if (_callback != NULL) {
+			_callback(topic, payload, length);
+		}
+	});
+	return _reconnect();
+}
+
+uint8_t iTeaMQTTClass::loop() {
+	if (_reconnect() == ITEA_STATE_MQTT_CONNECT) {
+		return ITEA_STATE_MQTT_CONNECT;
+	}
+	if (!_mqtt.loop()) {
+		return ITEA_STATE_MQTT_DISCONNECT;
+	}
+	unsigned long now = millis();
+	// customise frequency of heartbeat
+	if ((now < _lastHeartbeat) || (now - _lastHeartbeat) > 1000 * _heartbeatTick) {
+		// customise heartbeat topic
+		_mqtt.publish(_pubTopicHeartbeat, _clientId);
+		_lastHeartbeat = now;
+	}
+	return ITEA_STATE_RUN;
+}
+
+bool iTeaMQTTClass::publish(const char *topic, const char* payload) {
+	return _mqtt.publish(topic, payload);
+}
+
+uint8_t iTeaMQTTClass::_reconnect() {
+	if (_mqtt.connected()) {
+		return ITEA_STATE_MQTT_CONNECT;
+	}
+	int r;
+	if (strlen(_user) == 0) {
+		r = _mqtt.connect(_clientId);
+	} else {
+		r = _mqtt.connect(_clientId, _user, _pass);
+	}
+	if (r) {
+		_mqtt.subscribe(_subTopic);
+		return ITEA_STATE_MQTT_CONNECT;
+	}
+	return ITEA_STATE_MQTT_DISCONNECT;
+}
---
+++ src/libraries/iTea/iTeaMQTT.h
@@ -0,0 +1,46 @@
+#ifndef ITEA_MQTT
+#define ITEA_MQTT
+
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include "iTeaConfig.h"
+#include "iTeaHandler.h"
+
+#define ITEA_STATE_MQTT_CONNECT 10
+#define ITEA_STATE_MQTT_DISCONNECT 11
+
+// callback function 
+typedef void (*CallbackHandler)(const char*, const uint8_t*, unsigned int);
+
+// iTeaMQTT class
+class iTeaMQTTClass {
+	public:
+		iTeaMQTTClass();
+		void init(iTeaConfigClass *config, CallbackHandler callback);
+		
+		uint8_t setup();
+		uint8_t loop();
+
+		bool publish(const char* topic, const char* payload);
+	
+	private:
+		iTeaConfigClass *_config;
+		WiFiClientSecure _client;
+		PubSubClient _mqtt = PubSubClient(_client);
+
+		char _clientId[ITEA_MQTT_CLIENTID_SIZE + 1] = {0};
+		char _user[ITEA_MQTT_USER_SIZE + 1] = {0};
+		char _pass[ITEA_MQTT_PASS_SIZE + 1] = {0};		
+
+		char _pubTopicHeartbeat[ITEA_MQTT_TOPIC_SIZE + 1] = {0};
+		unsigned long _heartbeatTick = 60;
+		unsigned long _lastHeartbeat = 0;
+
+		char _subTopic[ITEA_MQTT_TOPIC_SIZE + 1] = {0};
+
+		CallbackHandler _callback = NULL;
+		uint8_t _reconnect();
+};
+
+static iTeaMQTTClass iTeaMQTT;
+#endif // ITEA_MQTT
