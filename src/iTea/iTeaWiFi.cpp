c3216a1c45031cee98f74e98bc93327412f47c2b
Xing Xing <mikespook@gmail.com>
2018-08-08 14:48:49 +1200

    better debug info, waiting enough time to connect to WiFi

--- src/iTea/iTeaWiFi.cpp
+++ src/iTea/iTeaWiFi.cpp
@@ -19,42 +19,39 @@ uint8_t iTeaWiFiClass::connect() {
 	
 	char ssid[ITEA_WIFI_SSID_SIZE] = {0};
 	_config->getSSID(ssid);
-	Serial.printf("SSID: %s\n", ssid);
 	char pass[ITEA_WIFI_PASS_SIZE] = {0};
 	_config->getPass(pass);
-	Serial.printf("Pass: %s\n", pass);
-	int a = WiFi.begin(ssid, pass);
-	Serial.printf("WIFI: %d\n", a);
-	return wait(100);
+	Serial.printf("[WiFi] Connecting: SSID=%s ", ssid);	
+	WiFi.begin(ssid, pass);
+	return wait(300);
 }
 
 uint8_t iTeaWiFiClass::wait(int tick) {
-	if (WiFi.isConnected()) {
-		return WL_CONNECTED;
-	}
 	String macStr = WiFi.macAddress();
 	memcpy(_macAddr, macStr.c_str(), 6);
-	// Wait max to tick/50 seconds
+	// Wait max to tick/10 seconds
 	for (int i = 0; i < tick; i ++) {
+		Serial.printf(".");
 		if (WiFi.isConnected()) {
-			Serial.printf("Connected: MAC=%s; SSID=%s; IP=", macStr.c_str(), WiFi.SSID().c_str());
+			Serial.printf("\n[WiFi] Connected: MAC=%s; SSID=%s; IP=", macStr.c_str(), WiFi.SSID().c_str());
 			Serial.println(WiFi.localIP());
 			return WL_CONNECTED;
 		}
+		delay(100);
 	}
 	int status = WiFi.status();
 	switch (status) {
 		case WL_NO_SSID_AVAIL:
-			Serial.printf("No SSID: MAC=%s; SSID=%s\n", macStr.c_str(), WiFi.SSID().c_str());
+			Serial.printf("[WiFi] No SSID: MAC=%s; SSID=%s\n", macStr.c_str(), WiFi.SSID().c_str());
 			break;
 		case WL_CONNECTION_LOST:
-			Serial.printf("Connection Lost: MAC=%s; SSID=%s\n", macStr.c_str(), WiFi.SSID().c_str());
+			Serial.printf("[WiFi] Connection Lost: MAC=%s; SSID=%s\n", macStr.c_str(), WiFi.SSID().c_str());
 			break;
 		case WL_CONNECT_FAILED:
-			Serial.printf("Connect Failed: MAC=%s; SSID=%s\n", macStr.c_str(), WiFi.SSID().c_str());
+			Serial.printf("[WiFi] Connect Failed: MAC=%s; SSID=%s\n", macStr.c_str(), WiFi.SSID().c_str());
 			break;
 		default:
-			Serial.printf("Connect Issue: MAC=%s; SSID=%s; Status=%d\n", macStr.c_str(), WiFi.SSID().c_str(), status);
+			Serial.printf("[WiFi] Connect Issue: MAC=%s; SSID=%s; Status=%d\n", macStr.c_str(), WiFi.SSID().c_str(), status);
 			break;
 	}
 	return status;
